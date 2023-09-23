3b9dd72b28284b9df935d73896a01166ebb8c70c
Sanford Freedman <sandy@sandmmakers.com>
2023-09-23 09:33:22 -0400

    Added WarningDialog and FatalErrorDialog TestApps

--- src/Dialogs/FatalErrorDialog.py
+++ src/Dialogs/FatalErrorDialog.py
@@ -2,5 +2,20 @@ from PySide6 import QtWidgets
 import sys
 
 def FatalErrorDialog(parent, message, errorCode=1):
+    """ Open an application modal fatal error dialog. This dialog will quit the entire application on close. """
     QtWidgets.QMessageBox.critical(parent, 'Fatal Error', message)
     sys.exit(errorCode)
+
+if __name__ == '__main__':
+    # Main only imports
+    from PySide6 import QtCore
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    QtCore.QCoreApplication.setApplicationName('FatalErrorDialog TestApp')
+
+    button = QtWidgets.QPushButton('Open')
+    button.clicked.connect(lambda : FatalErrorDialog(button, 'Test'))
+
+    button.show()
+    app.exec()
--- src/Dialogs/WarningDialog.py
+++ src/Dialogs/WarningDialog.py
@@ -1,4 +1,19 @@
 from PySide6 import QtWidgets
 
 def WarningDialog(parent, message):
+    """ Open an application modal warning dialog. """
     QtWidgets.QMessageBox.warning(parent, 'Warning', message)
+
+if __name__ == '__main__':
+    # Main only imports
+    from PySide6 import QtCore
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    QtCore.QCoreApplication.setApplicationName('WarningDialog TestApp')
+
+    button = QtWidgets.QPushButton('Open')
+    button.clicked.connect(lambda : WarningDialog(button, 'Test'))
+
+    button.show()
+    app.exec()
