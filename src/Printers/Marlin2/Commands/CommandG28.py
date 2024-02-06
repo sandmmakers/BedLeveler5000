7fb79512805ab616db6693f0e2e866657472d486
Sanford Freedman <sandy@sandmmakers.com>
2024-02-06 18:03:21 -0500

    Fixed CommandG28 bug treating 'R' argument as a bool

--- src/Printers/Marlin2/Commands/CommandG28.py
+++ src/Printers/Marlin2/Commands/CommandG28.py
@@ -1,9 +1,10 @@
 from .PositionOkCommand import PositionOkCommand
+from . import Converter
 
 class CommandG28(PositionOkCommand):
     NAME = 'G28'
 
-    def __init__(self, *, l=False, o=False, r=False, x=False, y=False, z=False):
+    def __init__(self, *, l=False, o=False, r=None, x=False, y=False, z=False):
         self.l = l
         self.o = o
         self.r = r
@@ -13,9 +14,10 @@ class CommandG28(PositionOkCommand):
 
         lPart = ' L' if self.l else ''
         oPart = ' O' if self.o else ''
-        rPart = ' R' if self.r else ''
+        rPart = '' if self.r is None else Converter.floatToStr(self.r, prefix=' R')
         xPart = ' X' if self.x else ''
         yPart = ' Y' if self.y else ''
         zPart = ' Z' if self.z else ''
 
         super().__init__(self.NAME + lPart + oPart + rPart + xPart + yPart + zPart)
+
